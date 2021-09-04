8ce22ef3a0e0d951d075d12f13282b10443d689c
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2021-09-04 13:04:51 -0400

    `CoreDataFeedStore.retrieve()` delivers found values on non-empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,7 +29,23 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let context = self.context
+		context.perform {
+			do {
+				let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+				request.returnsObjectsAsFaults = false
+				if let cache = try context.fetch(request).first {
+					completion(.found(feed: cache.feed
+							.compactMap { ($0 as? ManagedFeedImage) }
+							.map {
+								LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+							},
+						timestamp: cache.timestamp))
+				} else {
+					completion(.empty)
+				}
+			} catch {}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
