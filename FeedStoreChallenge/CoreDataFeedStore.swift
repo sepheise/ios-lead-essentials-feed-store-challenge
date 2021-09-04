633aaf0cd79e05462157cd0ca7c06bc859d8e553
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2021-09-04 14:26:35 -0400

    `CoreDataFeedStore.deleteCachedFeed()` empties previously inserted cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -61,7 +61,13 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		completion(nil)
+		let context = self.context
+		context.perform {
+			do {
+				try ManagedCache.find(in: context).map(context.delete).map(context.save)
+				completion(nil)
+			} catch {}
+		}
 	}
 }
 
