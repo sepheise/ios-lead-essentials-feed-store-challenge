e799435da13a484ec7a3145531eb2fc4283a48e3
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2021-09-04 15:27:30 -0400

    Change how context is captured inside the closure

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -71,8 +71,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
-		let context = self.context
-		context.perform { action(context) }
+		context.perform { [context] in action(context) }
 	}
 }
 
